9269f4de5e6adb590e9a470b73d7f35f5b71babc
mayorr-star <immaboat11@gmail.com>
2024-06-11 21:13:59 +0000

    Remove code to overwrite value from the appendNode function
    Good code readability and to make appendNode function responsible for only adding a new node

---
+++ src/linkedlist.js
@@ -0,0 +1,21 @@
+const createNode = require("./nodes");
+class linkedList {
+  constructor() {
+    this.headNode = null;
+  }
+
+  appendNode(key, value) {
+    const newNode = createNode(key, value);
+    if (this.headNode === null) {
+      this.headNode = newNode;
+    } else {
+      let tailNode = this.headNode;
+      while (tailNode.nextNode) {
+        tailNode = tailNode.nextNode;
+      }
+      tailNode.nextNode = newNode;
+    }
+  }
+}
+
+module.exports = linkedList;
