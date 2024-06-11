87296011b3474266a1625bfc03cf17cfae4f158e
mayorr-star <immaboat11@gmail.com>
2024-06-11 21:15:47 +0000

    Chane the way I export the factory function
    Good code readability

--- src/nodes.js
+++ src/nodes.js
@@ -1,7 +1,9 @@
-module.exports = function CreateNode(key = null, value = null, nextNode = null) {
+function createNode(key = null, value = null, nextNode = null) {
     return {
         key, 
         value,
         nextNode
     };
 };
+
+module.exports = createNode;
