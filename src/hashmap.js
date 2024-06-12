94e4601a520ced744557b5b9eee3826d025332b8
mayorr-star <immaboat11@gmail.com>
2024-06-12 12:49:27 +0000

    Capitalize the first letter of the name of the factory function
    Best practices for naming classes and contructor functions

--- src/hashmap.js
+++ src/hashmap.js
@@ -1,27 +1,22 @@
-const linkedList = require("./linkedlist");
-const createNode = require("./nodes");
+const LinkedList = require("./linkedlist");
 
-function createHashmap() {
-  const BUCKET_LIST = [];
-  BUCKET_LIST.length = 16;
-  for (let i = 0; i < BUCKET_LIST.length; i++) {
-    const list = new linkedList();
-    BUCKET_LIST[i] = list;
-  }
+function CreateHashmap() {
+  const bucketList = [];
+  bucketList.length = 16;
 
   const hash = (key) => {
     let hashCode = 0;
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode =
-        (primeNumber * hashCode + key.codePointAt(i)) % BUCKET_LIST.length;
+        (primeNumber * hashCode + key.codePointAt(i)) % bucketList.length;
     }
     return hashCode;
   };
 
   const overwriteValue = (key, newValue) => {
     const hashCode = hash(key);
-    let currentNode = BUCKET_LIST[hashCode].headNode;
+    let currentNode = bucketList[hashCode].headNode;
     while (currentNode) {
       if (currentNode.key === key) {
         currentNode.value = newValue;
@@ -32,16 +27,20 @@ function createHashmap() {
 
   const set = (key, value) => {
     if (has(key)) {
-      overwriteValue(key, value)
+      overwriteValue(key, value);
     } else {
       const hashCode = hash(key);
-      BUCKET_LIST[hashCode].appendNode(key, value);
+      if (bucketList[hashCode] === undefined) {
+        const list = new LinkedList();
+        bucketList[hashCode] = list;
+      }
+      bucketList[hashCode].appendNode(key, value);
     }
   };
 
   const get = (key) => {
     const hashCode = hash(key);
-    let pointer = BUCKET_LIST[hashCode].headNode;
+    let pointer = bucketList[hashCode].headNode;
     while (pointer) {
       if (key === pointer.key) return pointer.value;
       pointer = pointer.nextNode;
@@ -51,7 +50,8 @@ function createHashmap() {
 
   const has = (key) => {
     const hashCode = hash(key);
-    let pointer = BUCKET_LIST[hashCode].headNode;
+    if (!bucketList[hashCode]) return false;
+    let pointer = bucketList[hashCode].headNode;
     while (pointer) {
       if (key === pointer.key) return true;
       pointer = pointer.nextNode;
@@ -63,11 +63,11 @@ function createHashmap() {
     if (!has(key)) return false;
     const hashCode = hash(key);
     let previousPointer = null;
-    let pointer = BUCKET_LIST[hashCode].headNode;
+    let pointer = bucketList[hashCode].headNode;
     while (pointer) {
       if (pointer.key === key) {
         if (!previousPointer) {
-          BUCKET_LIST[hashCode].headNode = pointer.nextNode;
+          bucketList[hashCode].headNode = pointer.nextNode;
         } else {
           previousPointer.nextNode = pointer.nextNode;
         }
@@ -79,25 +79,77 @@ function createHashmap() {
 
   const length = () => {
     let numberOfKeys = 0;
-    for (const bucket of BUCKET_LIST) {
-      let currentNode = bucket.headNode;
-      while (currentNode !== null) {
-        numberOfKeys++;
-        currentNode = currentNode.nextNode;
+    for (const bucket of bucketList) {
+      if (bucket) {
+        let currentNode = bucket.headNode;
+        while (currentNode !== null) {
+          numberOfKeys++;
+          currentNode = currentNode.nextNode;
+        }
       }
     }
     return numberOfKeys;
+  };
+
+  const clear = () => {
+    return (bucketList.length = 0);
+  };
+
+  const keys = () => {
+    const keysStored = [];
+    for (const bucket of bucketList) {
+      if (bucket) {
+        let currentNode = bucket.headNode;
+        while (currentNode !== null) {
+          keysStored.push(currentNode.key);
+          currentNode = currentNode.nextNode;
+        }
+      }
+    }
+    return keysStored;
   }
+
+  const values = () => {
+    const valuesStored = [];
+    for (const bucket of bucketList) {
+      if (bucket) {
+        let currentNode = bucket.headNode;
+        while (currentNode !== null) {
+          valuesStored.push(currentNode.value);
+          currentNode = currentNode.nextNode;
+        }
+      }
+    }
+    return valuesStored;
+  }
+
+  const entries = () => {
+    const keyValuePair = [];
+    for (const bucket of bucketList) {
+      if (bucket) {
+        let currentNode = bucket.headNode;
+        while (currentNode !== null) {
+          keyValuePair.push([currentNode.key, currentNode.value]);
+          currentNode = currentNode.nextNode;
+        }
+      }
+    }
+    return keyValuePair;
+  }
+  
 
   return {
     hash,
     set,
-    BUCKET_LIST,
     get,
     has,
     remove,
     length,
+    clear,
+    keys,
+    values,
+    entries
   };
 }
 
-module.exports = createHashmap;
+module.exports = CreateHashmap;
